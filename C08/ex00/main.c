fa51c24be4dd2ed6be1775aa9d96fa7f6912b627
root <ky.claudan@gmail.com>
2024-10-04 17:32:03 +0000

    clean

---
+++ C08/ex00/main.c
@@ -0,0 +1,7 @@
+#include "ft.h"
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	printf("%d\n", ft_strcmp(argv[1], argv[2]));
+}
---
+++ C08/ex02/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "ft_abs.h"
+
+int	main(void)
+{
+	printf("%d", ABS(-25));
+}
